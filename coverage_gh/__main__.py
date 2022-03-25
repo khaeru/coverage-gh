9abd3245159c912f81b3b483ea90ab8ed72b9cb8
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-03-25 13:33:12 +0100

    Add metavar to CLI options

--- coverage_gh/__main__.py
+++ coverage_gh/__main__.py
@@ -7,11 +7,23 @@ from . import GitHubAPIClient
 # (non-functional), while allowing user overrides
 @click.command()
 @click.option(
-    "--api-url", envvar="GITHUB_API_URL", default="https://example.com/api/v999"
+    "--api-url",
+    envvar="GITHUB_API_URL",
+    metavar="GITHUB_API_URL",
+    default="https://example.com/api/v999",
 )
-@click.option("--pr-head-sha", envvar="PR_HEAD_SHA", default="a1b2c3d4")
-@click.option("--repo", envvar="GITHUB_REPOSITORY", default="user/repo")
-@click.option("--token", envvar="GITHUB_TOKEN", default="abc1234567890def")
+@click.option(
+    "--pr-head-sha", envvar="PR_HEAD_SHA", metavar="PR_HEAD_SHA", default="a1b2c3d4"
+)
+@click.option(
+    "--repo",
+    envvar="GITHUB_REPOSITORY",
+    metavar="GITHUB_REPOSITORY",
+    default="user/repo",
+)
+@click.option(
+    "--token", envvar="GITHUB_TOKEN", metavar="GITHUB_TOKEN", default="abc1234567890def"
+)
 @click.option("--verbose", is_flag=True, help="Display verbose output.")
 @click.option("--dry-run", is_flag=True, help="Only show what would be done.")
 @click.option(
